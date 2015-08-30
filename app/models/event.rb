4fa5cd38072093074a575fbaeb31ca86ef0b2502
sikheang <ung.sikheang@gmail.com>
2015-08-30 23:26:38 +0700

    commit

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,9 +1,5 @@
 class Event < ActiveRecord::Base
   belongs_to :origin
-  if Rails.env.development?
-    has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: 'default.png'
-    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-  else
     has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: 'default.png',
                       :storage => :dropbox,
                       :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
