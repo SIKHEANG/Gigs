085bd3db774b1ba915ec53754f880112673fcf84
sikheang <ung.sikheang@gmail.com>
2015-08-30 23:35:26 +0700

    add if else to event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,11 +1,16 @@
 class Event < ActiveRecord::Base
   belongs_to :origin
+
+  scope :khmer, -> { where(origin_id: 1).order('created_at DESC') }
+  scope :western, -> { where(origin_id: 2).order('created_at DESC') }
+
+  if Rails.env.development?
+    has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: 'default.png'
+  else
     has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: 'default.png',
                       :storage => :dropbox,
                       :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
                       :path => ":style/:id_:filename"
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
-
-    scope :khmer, -> { where(origin_id: 1).order('created_at DESC') }
-    scope :western, -> { where(origin_id: 2).order('created_at DESC') }
   end
+end
