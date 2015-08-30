ea138d45d4c3df2d9152d142f91abca01b81c5e7
sikheang <ung.sikheang@gmail.com>
2015-08-30 22:36:44 +0700

    add image code back to home page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,13 @@
 class Event < ActiveRecord::Base
   belongs_to :origin
-  has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: "/images/:style/missing.png",
-                    :storage => :dropbox,
-                    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  if Rails.env.development?
+    has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: 'default.png'
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  else
+    has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: 'default.png',
+                      :storage => :dropbox,
+                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                      :path => ":style/:id_:filename"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 
   scope :khmer, -> { where(origin_id: 1).order('created_at DESC') }
--- app/models/musician.rb
+++ app/models/musician.rb
@@ -1,7 +1,12 @@
 class Musician < ActiveRecord::Base
   belongs_to :origin
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" },
-                    :storage => :dropbox,
-                    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+  if Rails.env.development?
+    has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: 'default.png'
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+  else
+    has_attached_file :image, styles: { medium: "200x200>", thumb: "100x100>" }, default_url: 'default.png',
+                      :storage => :dropbox,
+                      :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+                      :path => ":style/:id_:filename"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
