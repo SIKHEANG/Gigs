7549b765b5b50a95c0744bf988a14acc27527adf
sikheang <ung.sikheang@gmail.com>
2015-09-01 14:10:04 +0700

    Fixed musician_controller for check_user

--- app/controllers/musicians_controller.rb
+++ app/controllers/musicians_controller.rb
@@ -1,7 +1,7 @@
 class MusiciansController < ApplicationController
   before_action :set_musician, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:show, :index]
-  before_action :check_user, only: [:new, :edit, :update, :destroy]
+  before_action :check_user, only: [:edit, :update, :destroy]
 
   # GET /musicians
   # GET /musicians.json
