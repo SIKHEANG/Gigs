2d66dcca3114cc20408db29d201d83b1fc0b08f3
sikheang <ung.sikheang@gmail.com>
2015-09-13 13:04:28 +0700

    delete erb syntax to production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,10 +87,10 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
   address: "smtp.gmail.com",
   port: 587,
-  domain: http://uskgigs.herokuapp.com/
+  domain: "http://uskgigs.herokuapp.com/",
   authentication: "plain",
   enable_starttls_auto: true,
-  user_name: "uskgigs@gmail.com"
+  user_name: "uskgigs@gmail.com",
   password: "Uskgigs2015"
   }
 end
